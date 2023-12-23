4010c26cbe3c65236f7a702ca3784b657819e47b
Pawel Wilczewski <pawel.wilczewski@outlook.com>
2023-12-23 02:59:04 +0100

    Day 2 solution

--- src/Day2/GameValidityChecker.cs
+++ src/Day2/GameValidityChecker.cs
@@ -48,35 +48,65 @@ public static class GameValidityChecker
 
 				return result;
 			}
+
+			public override string ToString()
+			{
+				return $"red {Red}, green {Green}, blue {Blue}";
+			}
 		}
 
-		public int Id { get; init; }
-		public GemsInfo BagConfig { get; init; }
+		public int Id { get; private init; }
+		public GemsInfo BagConfig { get; private set; }
 
-		public List<GemsInfo> GemsShowed { get; init; } = [];
-	}
+		public List<GemsInfo> GemsShowed { get; private init; } = [];
 
-	private static List<GameInfo> ProcessInput(string bagConfiguration, string input)
-	{
-		var result = new List<GameInfo>();
-		foreach (var gameInfo in input.ReplaceLineEndings("\n").Split("\n"))
+		private void RecalculateMinBagConfiguration()
 		{
-			var splitInfo = gameInfo.Split(": ");
-			result.Add(new GameInfo
+			BagConfig = new GemsInfo
+			{
+				Red = GemsShowed.Max(info => info.Red),
+				Green = GemsShowed.Max(info => info.Green),
+				Blue = GemsShowed.Max(info => info.Blue)
+			};
+		}
+
+		public static GameInfo Deserialize(string text, string? bagConfiguration)
+		{
+			var splitInfo = text.Split(": ");
+			var result = new GameInfo
 			{
 				Id = int.Parse(splitInfo[0].Split(' ')[1]),
-				BagConfig = GameInfo.GemsInfo.Deserialize(bagConfiguration),
-				GemsShowed = splitInfo[1].Split("; ").Select(GameInfo.GemsInfo.Deserialize).ToList()
-			});
-		}
+				GemsShowed = splitInfo[1].Split("; ").Select(GemsInfo.Deserialize).ToList()
+			};
 
-		return result;
+			if (bagConfiguration is not null)
+			{
+				result.BagConfig = GemsInfo.Deserialize(bagConfiguration);
+			}
+			else
+			{
+				result.RecalculateMinBagConfiguration();
+			}
+
+			return result;
+		}
 	}
 
-	public static int EvaluateValidGamesIdSum(string bagConfiguration, string input)
+	private static IEnumerable<GameInfo> ProcessInput(string input, string? bagConfiguration = null)
 	{
-		return ProcessInput(bagConfiguration, input)
+		return input.ReplaceLineEndings("\n").Split("\n")
+					.Select(gameInfo => GameInfo.Deserialize(gameInfo, bagConfiguration));
+	}
+
+	public static int EvaluateValidGamesIdSum(string input, string bagConfiguration)
+	{
+		return ProcessInput(input, bagConfiguration)
 				.Where(game => game.GemsShowed.TrueForAll(x => !(x > game.BagConfig)))
 				.Sum(game => game.Id);
-	
+	}
+
+	public static int EvaluatePowerOfMinCubesSum(string input)
+	{
+		return ProcessInput(input).Sum(game => game.BagConfig.Red * game.BagConfig.Green * game.BagConfig.Blue);
+	}
 }
